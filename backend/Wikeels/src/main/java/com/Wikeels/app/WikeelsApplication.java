30ce4beaf96c63c27c13fbebdc2e2b3767307542
rare-sight <rare-sight>
2025-03-23 12:46:45 +0530

    Spring initialized

---
+++ backend/Wikeels/src/main/java/com/Wikeels/app/WikeelsApplication.java
@@ -0,0 +1,13 @@
+package com.Wikeels.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WikeelsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WikeelsApplication.class, args);
+	}
+
+}
---
+++ backend/Wikeels/src/test/java/com/Wikeels/app/WikeelsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Wikeels.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WikeelsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
